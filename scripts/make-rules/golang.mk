ce4b70ee695fc2601861c547ee47b656e86660e8
rose839 <617418745@qq.com>
2023-01-13 19:20:43 +0800

    refactor(script): 修改golang.mk

--- scripts/make-rules/golang.mk
+++ scripts/make-rules/golang.mk
@@ -8,3 +8,17 @@ GO_LDFLAGS += -X $(VERSION_PACKAGE).GitVersion=$(VERSION) \
 	-X $(VERSION_PACKAGE).GitCommit=$(GIT_COMMIT) \
 	-X $(VERSION_PACKAGE).GitTreeState=$(GIT_TREE_STATE) \
 	-X $(VERSION_PACKAGE).BuildDate=$(shell date -u +'%Y-%m-%dT%H:%M:%SZ')
+ifneq ($(DLV),)
+	# 调试模式下关闭编译优化
+	GO_BUILD_FLAGS += -gcflags "all=-N -l"
+	LDFLAGS = ""
+endif
+GO_BUILD_FLAGS += -tags=jsoniter -ldflags "$(GO_LDFLAGS)"
+
+ifeq ($(GOOS), windows)
+	GO_OUT_EXE := .exe
+endif
+
+ifeq ($(ROOT_PACKAGE),)
+	$(error the variable ROOT_PACKAGE must be set prior to including golang.mk)
+endif
